5a204ef73884228dbf982b489d9c4b19cc53db5d
nabila hajou <nab.ikk13@gmail.com>
2021-11-05 21:49:31 +0100

    finish design and js of niveaux page

---
+++ niveaux.js
@@ -0,0 +1,59 @@
+let niveauA1 = document.getElementById("niveauA1");
+
+niveauA1.addEventListener("mouseover", () => {
+  niveauA1.style.transform = "translate(-10px,0px)";
+});
+
+niveauA1.addEventListener("mouseout", () => {
+  niveauA1.style.transform = "translate(0px,0px)";
+});
+
+let niveauA2 = document.getElementById("niveauA2");
+
+niveauA2.addEventListener("mouseover", () => {
+  niveauA2.style.transform = "translate(10px,0px)";
+});
+
+niveauA2.addEventListener("mouseout", () => {
+  niveauA2.style.transform = "translate(0px,0px)";
+});
+
+let niveauB1 = document.getElementById("niveauB1");
+
+niveauB1.addEventListener("mouseover", () => {
+  niveauB1.style.transform = "translate(-10px,0px)";
+});
+
+niveauB1.addEventListener("mouseout", () => {
+  niveauB1.style.transform = "translate(0px,0px)";
+});
+
+let niveauB2 = document.getElementById("niveauB2");
+
+niveauB2.addEventListener("mouseover", () => {
+  niveauB2.style.transform = "translate(10px,0px)";
+});
+
+niveauB2.addEventListener("mouseout", () => {
+  niveauB2.style.transform = "translate(0px,0px)";
+});
+
+let niveauC1 = document.getElementById("niveauC1");
+
+niveauC1.addEventListener("mouseover", () => {
+  niveauC1.style.transform = "translate(-10px,0px)";
+});
+
+niveauC1.addEventListener("mouseout", () => {
+  niveauC1.style.transform = "translate(0px,0px)";
+});
+
+let niveauC2 = document.getElementById("niveauC2");
+
+niveauC2.addEventListener("mouseover", () => {
+  niveauC2.style.transform = "translate(10px,0px)";
+});
+
+niveauC2.addEventListener("mouseout", () => {
+  niveauC2.style.transform = "translate(0px,0px)";
+});
