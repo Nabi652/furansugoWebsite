2d91f045e799058dced46bbe011830e8ac7984dd
nabila hajou <nab.ikk13@gmail.com>
2022-01-26 05:01:39 +0100

    added audios to js file niveaulecon1.js

--- niveauA1lecon1.js
+++ niveauA1lecon1.js
@@ -1,15 +1,44 @@
-/* audio pour a-l'alphabet */
+/* audio pour LETTRE A */
 
-// let audioAlphabet = document.getElementById("audioAlphabet");
-// let titreAlphabet = document.getElementById("titreAlphabet");
-
-// titreAlphabet.addEventListener("mouseover", () => {
-//   audioAlphabet.play();
-// });
-
-let lettre = document.getElementById("letterA");
+let lettreA = document.getElementById("letterA");
 let audioA = document.getElementById("audioA");
 
-lettre.addEventListener("click", () => {
+lettreA.addEventListener("click", () => {
   audioA.play();
+});
+
+/* audio pour LETTRE B */
+
+let lettreB = document.getElementById("letterB");
+let audioB = document.getElementById("audioB");
+
+lettreB.addEventListener("click", () => {
+  audioB.play();
+});
+
+/* audio pour LETTRE C */
+
+let lettreC = document.getElementById("letterC");
+let audioC = document.getElementById("audioC");
+
+lettreC.addEventListener("click", () => {
+  audioC.play();
+});
+
+/* audio pour LETTRE D */
+
+let lettreD = document.getElementById("letterD");
+let audioD = document.getElementById("audioD");
+
+lettreD.addEventListener("click", () => {
+  audioD.play();
+});
+
+/* audio pour LETTRE E */
+
+let lettreE = document.getElementById("letterE");
+let audioE = document.getElementById("audioE");
+
+lettreE.addEventListener("click", () => {
+  audioE.play();
 });
