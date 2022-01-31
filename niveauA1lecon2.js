bb17923fe4c89f84f92deb07839e44e5a96e08a1
nabila hajou <nab.ikk13@gmail.com>
2022-01-31 08:28:39 +0100

    added audio of sePresenter part of niveauA1lecon2 js file

---
+++ niveauA1lecon2.js
@@ -0,0 +1,162 @@
+/* SE PRESENTER*/
+/* audio pour "je m'appelle.." */
+
+let lettreSePresenter1 = document.getElementById("sePresenter1");
+let audioSePresenter1 = document.getElementById("audioSePresenter1");
+
+lettreSePresenter1.addEventListener("click", () => {
+  audioSePresenter1.play();
+});
+
+/* audio pour "j'ai..ans" */
+
+let lettreSePresenter2 = document.getElementById("sePresenter2");
+let audioSePresenter2 = document.getElementById("audioSePresenter2");
+
+lettreSePresenter2.addEventListener("click", () => {
+  audioSePresenter2.play();
+});
+
+/* audio pour "je viens de/du/des.." */
+
+let lettreSePresenter3 = document.getElementById("sePresenter3");
+let audioSePresenter3 = document.getElementById("audioSePresenter3");
+
+lettreSePresenter3.addEventListener("click", () => {
+  audioSePresenter3.play();
+});
+
+/* audio pour "suis français/japonais/italien.." */
+
+let lettreSePresenter4 = document.getElementById("sePresenter4");
+let audioSePresenter4 = document.getElementById("audioSePresenter4");
+
+lettreSePresenter4.addEventListener("click", () => {
+  audioSePresenter4.play();
+});
+
+/* audio pour "j'habite à..." */
+
+let lettreSePresenter5 = document.getElementById("sePresenter5");
+let audioSePresenter5 = document.getElementById("audioSePresenter5");
+
+lettreSePresenter5.addEventListener("click", () => {
+  audioSePresenter5.play();
+});
+
+/* audio pour "je m'appelle..Je suis marié(男)/mariée(女)*/
+
+let lettreSePresenter6 = document.getElementById("sePresenter6");
+let audioSePresenter6 = document.getElementById("audioSePresenter6");
+
+lettreSePresenter6.addEventListener("click", () => {
+  audioSePresenter6.play();
+});
+
+/* audio pour "j'ai un enfant"*/
+
+let lettreSePresenter7 = document.getElementById("sePresenter7");
+let audioSePresenter7 = document.getElementById("audioSePresenter7");
+
+lettreSePresenter7.addEventListener("click", () => {
+  audioSePresenter7.play();
+});
+
+/* audio pour "je n'ai pas d'enfant"*/
+
+let lettreSePresenter8 = document.getElementById("sePresenter8");
+let audioSePresenter8 = document.getElementById("audioSePresenter8");
+
+lettreSePresenter8.addEventListener("click", () => {
+  audioSePresenter8.play();
+});
+
+/* audio pour "j'ai un frere"*/
+
+let lettreSePresenter9 = document.getElementById("sePresenter9");
+let audioSePresenter9 = document.getElementById("audioSePresenter9");
+
+lettreSePresenter9.addEventListener("click", () => {
+  audioSePresenter9.play();
+});
+
+/* audio pour "j'ai une soeur"*/
+
+let lettreSePresenter10 = document.getElementById("sePresenter10");
+let audioSePresenter10 = document.getElementById("audioSePresenter10");
+
+lettreSePresenter10.addEventListener("click", () => {
+  audioSePresenter10.play();
+});
+
+/* audio pour "je suis fils unique "*/
+
+let lettreSePresenter11 = document.getElementById("sePresenter11");
+let audioSePresenter11 = document.getElementById("audioSePresenter11");
+
+lettreSePresenter11.addEventListener("click", () => {
+  audioSePresenter11.play();
+});
+
+/* audio pour "je suis étudiant"*/
+
+let lettreSePresenter12 = document.getElementById("sePresenter12");
+let audioSePresenter12 = document.getElementById("audioSePresenter12");
+
+lettreSePresenter12.addEventListener("click", () => {
+  audioSePresenter12.play();
+});
+
+/* audio pour "je suis ecolier/collegien..."*/
+
+let lettreSePresenter13 = document.getElementById("sePresenter13");
+let audioSePresenter13 = document.getElementById("audioSePresenter13");
+
+lettreSePresenter13.addEventListener("click", () => {
+  audioSePresenter13.play();
+});
+
+/* audio pour "je travaille comme/en tant que..."*/
+
+let lettreSePresenter14 = document.getElementById("sePresenter14");
+let audioSePresenter14 = document.getElementById("audioSePresenter14");
+
+lettreSePresenter14.addEventListener("click", () => {
+  audioSePresenter14.play();
+});
+
+/* audio pour "je suis au chomage"*/
+
+let lettreSePresenter15 = document.getElementById("sePresenter15");
+let audioSePresenter15 = document.getElementById("audioSePresenter15");
+
+lettreSePresenter15.addEventListener("click", () => {
+  audioSePresenter15.play();
+});
+
+/* audio pour "j'ai quitté mon travail"*/
+
+let lettreSePresenter16 = document.getElementById("sePresenter16");
+let audioSePresenter16 = document.getElementById("audioSePresenter16");
+
+lettreSePresenter16.addEventListener("click", () => {
+  audioSePresenter16.play();
+});
+
+/* audio pour "je suis a la retaite"*/
+
+let lettreSePresenter17 = document.getElementById("sePresenter17");
+let audioSePresenter17 = document.getElementById("audioSePresenter17");
+
+lettreSePresenter17.addEventListener("click", () => {
+  audioSePresenter17.play();
+});
+
+/* audio pour "je suis en france depuis 1 an"*/
+
+let lettreSePresenter18 = document.getElementById("sePresenter18");
+let audioSePresenter18 = document.getElementById("audioSePresenter18");
+
+lettreSePresenter18.addEventListener("click", () => {
+  audioSePresenter18.play();
+});
