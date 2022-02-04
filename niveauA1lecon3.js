1b8446f3457d76c4e4efb79a79e2a17e9908c307
nabila hajou <nab.ikk13@gmail.com>
2022-02-04 06:15:40 +0100

    added all audios of famille table in niveauA1lecon3 js file

---
+++ niveauA1lecon3.js
@@ -0,0 +1,254 @@
+/* audios pour le tableau famille */
+
+/* le père*/
+
+let familleLePere = document.getElementById("familleLePere");
+let audioFamilleLePere = document.getElementById("audioFamilleLePere");
+
+familleLePere.addEventListener("click", () => {
+  audioFamilleLePere.play();
+});
+
+/* la mere */
+
+let familleLaMere = document.getElementById("familleLaMere");
+let audioFamilleLaMere = document.getElementById("audioFamilleLaMere");
+
+familleLaMere.addEventListener("click", () => {
+  audioFamilleLaMere.play();
+});
+
+/* le frère */
+
+let familleLeFrere = document.getElementById("familleLeFrere");
+let audioFamilleLeFrere = document.getElementById("audioFamilleLeFrere");
+
+familleLeFrere.addEventListener("click", () => {
+  audioFamilleLeFrere.play();
+});
+
+/* la soeur */
+
+let familleLaSoeur = document.getElementById("familleLaSoeur");
+let audioFamilleLaSoeur = document.getElementById("audioFamilleLaSoeur");
+
+familleLaSoeur.addEventListener("click", () => {
+  audioFamilleLaSoeur.play();
+});
+
+/* le cousin */
+
+let familleLeCousin = document.getElementById("familleLeCousin");
+let audioFamilleLeCousin = document.getElementById("audioFamilleLeCousin");
+
+familleLeCousin.addEventListener("click", () => {
+  audioFamilleLeCousin.play();
+});
+
+/* la cousine*/
+
+let familleLaCousine = document.getElementById("familleLaCousine");
+let audioFamilleLaCousine = document.getElementById("audioFamilleLaCousine");
+
+familleLaCousine.addEventListener("click", () => {
+  audioFamilleLaCousine.play();
+});
+
+/* l'oncle' */
+
+let familleLoncle = document.getElementById("familleLoncle");
+let audioFamilleLoncle = document.getElementById("audioFamilleLoncle");
+
+familleLoncle.addEventListener("click", () => {
+  audioFamilleLoncle.play();
+});
+
+/* la tante*/
+
+let familleLaTante = document.getElementById("familleLaTante");
+let audioFamilleLaTante = document.getElementById("audioFamilleLaTante");
+
+familleLaTante.addEventListener("click", () => {
+  audioFamilleLaTante.play();
+});
+
+/* le neveu */
+
+let familleLeNeveu = document.getElementById("familleLeNeveu");
+let audioFamilleLeNeveu = document.getElementById("audioFamilleLeNeveu");
+
+familleLeNeveu.addEventListener("click", () => {
+  audioFamilleLeNeveu.play();
+});
+
+/* la niece */
+
+let familleLaNiece = document.getElementById("familleLaNiece");
+let audioFamilleLaNiece = document.getElementById("audioFamilleLaNiece");
+
+familleLaNiece.addEventListener("click", () => {
+  audioFamilleLaNiece.play();
+});
+
+/* le grand pere */
+
+let familleLeGrandPere = document.getElementById("familleLeGrandPere");
+let audioFamilleLeGrandPere = document.getElementById(
+  "audioFamilleLeGrandPere"
+);
+
+familleLeGrandPere.addEventListener("click", () => {
+  audioFamilleLeGrandPere.play();
+});
+
+/* la grand mère */
+
+let familleLaGrandMere = document.getElementById("familleLaGrandMere");
+let audioFamilleLaGrandMere = document.getElementById(
+  "audioFamilleLaGrandMere"
+);
+
+familleLaGrandMere.addEventListener("click", () => {
+  audioFamilleLaGrandMere.play();
+});
+
+/* le beau pere */
+
+let familleLeBeauPere = document.getElementById("familleLeBeauPere");
+let audioFamilleLeBeauPere = document.getElementById("audioFamilleLeBeauPere");
+
+familleLeBeauPere.addEventListener("click", () => {
+  audioFamilleLeBeauPere.play();
+});
+
+/* la belle mere*/
+
+let familleLaBelleMere = document.getElementById("familleLaBelleMere");
+let audioFamilleLaBelleMere = document.getElementById(
+  "audioFamilleLaBelleMere"
+);
+
+familleLaBelleMere.addEventListener("click", () => {
+  audioFamilleLaBelleMere.play();
+});
+
+/* le beau fils*/
+
+let familleLeBeauFils = document.getElementById("familleLeBeauFils");
+let audioFamilleLeBeauFils = document.getElementById("audioFamilleLeBeauFils");
+
+familleLeBeauFils.addEventListener("click", () => {
+  audioFamilleLeBeauFils.play();
+});
+
+/* la belle fille */
+
+let familleLaBelleFille = document.getElementById("familleLaBelleFille");
+let audioFamilleLaBelleFille = document.getElementById(
+  "audioFamilleLaBelleFille"
+);
+
+familleLaBelleFille.addEventListener("click", () => {
+  audioFamilleLaBelleFille.play();
+});
+
+/* le  fils*/
+
+let familleLeFils = document.getElementById("familleLeFils");
+let audioFamilleLeFils = document.getElementById("audioFamilleLeFils");
+
+familleLeFils.addEventListener("click", () => {
+  audioFamilleLeFils.play();
+});
+
+/* la fille*/
+
+let familleLaFille = document.getElementById("familleLaFille");
+let audioFamilleLaFille = document.getElementById("audioFamilleLaFille");
+
+familleLaFille.addEventListener("click", () => {
+  audioFamilleLaFille.play();
+});
+
+/* les parents*/
+
+let familleLesParents = document.getElementById("familleLesParents");
+let audioFamilleLesParents = document.getElementById("audioFamilleLesParents");
+
+familleLesParents.addEventListener("click", () => {
+  audioFamilleLesParents.play();
+});
+
+/* les grands parents*/
+
+let familleLesGrandsParents = document.getElementById(
+  "familleLesGrandsParents"
+);
+let audioFamilleLesGrandsParents = document.getElementById(
+  "audioFamilleLesGrandsParents"
+);
+
+familleLesGrandsParents.addEventListener("click", () => {
+  audioFamilleLesGrandsParents.play();
+});
+
+/* les grands parents paternels*/
+
+let familleLesGrandsParentsPaternels = document.getElementById(
+  "familleLesGrandsParentsPaternels"
+);
+let audioFamilleLesGrandsParentsPaternels = document.getElementById(
+  "audioFamilleLesGrandsParentsPaternels"
+);
+
+familleLesGrandsParentsPaternels.addEventListener("click", () => {
+  audioFamilleLesGrandsParentsPaternels.play();
+});
+
+/* les grands parents maternels*/
+
+let familleLesGrandsParentsMaternels = document.getElementById(
+  "familleLesGrandsParentsMaternels"
+);
+let audioFamilleLesGrandsParentsMaternels = document.getElementById(
+  "audioFamilleLesGrandsParentsMaternels"
+);
+
+familleLesGrandsParentsMaternels.addEventListener("click", () => {
+  audioFamilleLesGrandsParentsMaternels.play();
+});
+
+/* les freres et soeurs*/
+
+let familleLesFreresEtSoeurs = document.getElementById(
+  "familleLesFreresEtSoeurs"
+);
+let audioFamilleLesFreresEtSoeurs = document.getElementById(
+  "audioFamilleLesFreresEtSoeurs"
+);
+
+familleLesFreresEtSoeurs.addEventListener("click", () => {
+  audioFamilleLesFreresEtSoeurs.play();
+});
+
+/* le grand oncle*/
+
+let familleLeGrandOncle = document.getElementById("familleLeGrandOncle");
+let audioFamilleLeGrandOncle = document.getElementById(
+  "audioFamilleLeGrandOncle"
+);
+
+familleLeGrandOncle.addEventListener("click", () => {
+  audioFamilleLeGrandOncle.play();
+});
+
+/* la grande tante*/
+
+let familleLaGrandeTante = document.getElementById("familleLaGrandeTante");
+let audioFamilleLaGrandeTante = document.getElementById(
+  "audioFamilleLaGrandeTante"
+);
+
+familleLaGrandeTante.addEventListener("click", () => {
+  audioFamilleLaGrandeTante.play();
+});
