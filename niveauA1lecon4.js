b36077017a1d910028d41eb8b09ca3259a1af2cb
nabila hajou <nab.ikk13@gmail.com>
2022-02-10 05:25:58 +0100

    added audio via js niveaua1lecon4 js file

---
+++ niveauA1lecon4.js
@@ -0,0 +1,218 @@
+/* exemples introduction present de l'indicatif */
+
+let presentExemple1 = document.getElementById("presentExemple1");
+let audioPresentExemple1 = document.getElementById("audioPresentExemple1");
+
+presentExemple1.addEventListener("click", () => {
+  audioPresentExemple1.play();
+});
+
+let buttonStop1 = document.getElementById("buttonStop1");
+
+buttonStop1.addEventListener("click", () => {
+  audioPresentExemple1.pause();
+  audioPresentExemple1.currentTime = 0;
+});
+
+let presentExemple2 = document.getElementById("presentExemple2");
+let audioPresentExemple2 = document.getElementById("audioPresentExemple2");
+
+presentExemple2.addEventListener("click", () => {
+  audioPresentExemple2.play();
+});
+
+let buttonStop2 = document.getElementById("buttonStop2");
+
+buttonStop2.addEventListener("click", () => {
+  audioPresentExemple2.pause();
+  audioPresentExemple2.currentTime = 0;
+});
+
+let presentExemple3 = document.getElementById("presentExemple3");
+let audioPresentExemple3 = document.getElementById("audioPresentExemple3");
+
+presentExemple3.addEventListener("click", () => {
+  audioPresentExemple3.play();
+});
+
+let buttonStop3 = document.getElementById("buttonStop3");
+
+buttonStop3.addEventListener("click", () => {
+  audioPresentExemple3.pause();
+  audioPresentExemple3.currentTime = 0;
+});
+
+let presentExemple4 = document.getElementById("presentExemple4");
+let audioPresentExemple4 = document.getElementById("audioPresentExemple4");
+
+presentExemple4.addEventListener("click", () => {
+  audioPresentExemple4.play();
+});
+
+let buttonStop4 = document.getElementById("buttonStop4");
+
+buttonStop4.addEventListener("click", () => {
+  audioPresentExemple4.pause();
+  audioPresentExemple4.currentTime = 0;
+});
+
+let presentExemple5 = document.getElementById("presentExemple5");
+let audioPresentExemple5 = document.getElementById("audioPresentExemple5");
+
+presentExemple5.addEventListener("click", () => {
+  audioPresentExemple5.play();
+});
+
+let buttonStop5 = document.getElementById("buttonStop5");
+
+buttonStop5.addEventListener("click", () => {
+  audioPresentExemple5.pause();
+  audioPresentExemple5.currentTime = 0;
+});
+
+/* exemples verbes du premier groupe */
+
+let premierGroupeExemple1 = document.getElementById("premierGroupeExemple1");
+let audioPremierGroupeExemple1 = document.getElementById(
+  "audioPremierGroupeExemple1"
+);
+
+premierGroupeExemple1.addEventListener("click", () => {
+  audioPremierGroupeExemple1.play();
+});
+
+let buttonStop6 = document.getElementById("buttonStop6");
+
+buttonStop6.addEventListener("click", () => {
+  audioPremierGroupeExemple1.pause();
+  audioPremierGroupeExemple1.currentTime = 0;
+});
+
+let premierGroupeExemple2 = document.getElementById("premierGroupeExemple2");
+let audioPremierGroupeExemple2 = document.getElementById(
+  "audioPremierGroupeExemple2"
+);
+
+premierGroupeExemple2.addEventListener("click", () => {
+  audioPremierGroupeExemple2.play();
+});
+
+let buttonStop7 = document.getElementById("buttonStop7");
+
+buttonStop7.addEventListener("click", () => {
+  audioPremierGroupeExemple2.pause();
+  audioPremierGroupeExemple2.currentTime = 0;
+});
+
+let premierGroupeExemple3 = document.getElementById("premierGroupeExemple3");
+let audioPremierGroupeExemple3 = document.getElementById(
+  "audioPremierGroupeExemple3"
+);
+
+premierGroupeExemple3.addEventListener("click", () => {
+  audioPremierGroupeExemple3.play();
+});
+
+let buttonStop8 = document.getElementById("buttonStop8");
+
+buttonStop8.addEventListener("click", () => {
+  audioPremierGroupeExemple3.pause();
+  audioPremierGroupeExemple3.currentTime = 0;
+});
+
+let premierGroupeExemple4 = document.getElementById("premierGroupeExemple4");
+let audioPremierGroupeExemple4 = document.getElementById(
+  "audioPremierGroupeExemple4"
+);
+
+premierGroupeExemple4.addEventListener("click", () => {
+  audioPremierGroupeExemple4.play();
+});
+
+let buttonStop9 = document.getElementById("buttonStop9");
+
+buttonStop9.addEventListener("click", () => {
+  audioPremierGroupeExemple4.pause();
+  audioPremierGroupeExemple4.currentTime = 0;
+});
+
+let premierGroupeExemple5 = document.getElementById("premierGroupeExemple5");
+let audioPremierGroupeExemple5 = document.getElementById(
+  "audioPremierGroupeExemple5"
+);
+
+premierGroupeExemple5.addEventListener("click", () => {
+  audioPremierGroupeExemple5.play();
+});
+
+let buttonStop10 = document.getElementById("buttonStop10");
+
+buttonStop10.addEventListener("click", () => {
+  audioPremierGroupeExemple5.pause();
+  audioPremierGroupeExemple5.currentTime = 0;
+});
+
+/* verbe manger */
+
+let manger1 = document.getElementById("manger1");
+let audioManger1 = document.getElementById("audioManger1");
+
+manger1.addEventListener("click", () => {
+  audioManger1.play();
+});
+
+let manger2 = document.getElementById("manger2");
+let audioManger2 = document.getElementById("audioManger2");
+
+manger2.addEventListener("click", () => {
+  audioManger2.play();
+});
+
+let manger3 = document.getElementById("manger3");
+let audioManger3 = document.getElementById("audioManger3");
+
+manger3.addEventListener("click", () => {
+  audioManger3.play();
+});
+
+let manger4 = document.getElementById("manger4");
+let audioManger4 = document.getElementById("audioManger4");
+
+manger4.addEventListener("click", () => {
+  audioManger4.play();
+});
+
+let manger5 = document.getElementById("manger5");
+let audioManger5 = document.getElementById("audioManger5");
+
+manger5.addEventListener("click", () => {
+  audioManger5.play();
+});
+
+let manger6 = document.getElementById("manger6");
+let audioManger6 = document.getElementById("audioManger6");
+
+manger6.addEventListener("click", () => {
+  audioManger6.play();
+});
+
+let manger7 = document.getElementById("manger7");
+let audioManger7 = document.getElementById("audioManger7");
+
+manger7.addEventListener("click", () => {
+  audioManger7.play();
+});
+
+let manger8 = document.getElementById("manger8");
+let audioManger8 = document.getElementById("audioManger8");
+
+manger8.addEventListener("click", () => {
+  audioManger8.play();
+});
+
+let manger9 = document.getElementById("manger9");
+let audioManger9 = document.getElementById("audioManger9");
+
+manger9.addEventListener("click", () => {
+  audioManger9.play();
+});
