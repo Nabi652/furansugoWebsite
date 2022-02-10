d65bae916e09bad810fd7c6013ba12275eaf3899
nabila hajou <nab.ikk13@gmail.com>
2022-02-10 08:45:47 +0100

    added audios to asset folder

--- niveauA1lecon4.js
+++ niveauA1lecon4.js
@@ -216,3 +216,390 @@ let audioManger9 = document.getElementById("audioManger9");
 manger9.addEventListener("click", () => {
   audioManger9.play();
 });
+
+/* exemples deuxieme groupe */
+
+let deuxiemeGroupeExemple1 = document.getElementById("deuxiemeGroupeExemple1");
+let audioDeuxiemeGroupeExemple1 = document.getElementById(
+  "audioDeuxiemeGroupeExemple1"
+);
+
+deuxiemeGroupeExemple1.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple1.play();
+});
+
+let buttonStop11 = document.getElementById("buttonStop11");
+
+buttonStop11.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple1.pause();
+  audioDeuxiemeGroupeExemple1.currentTime = 0;
+});
+
+let deuxiemeGroupeExemple2 = document.getElementById("deuxiemeGroupeExemple2");
+let audioDeuxiemeGroupeExemple2 = document.getElementById(
+  "audioDeuxiemeGroupeExemple2"
+);
+
+deuxiemeGroupeExemple2.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple2.play();
+});
+
+let buttonStop12 = document.getElementById("buttonStop12");
+
+buttonStop12.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple2.pause();
+  audioDeuxiemeGroupeExemple2.currentTime = 0;
+});
+
+let deuxiemeGroupeExemple3 = document.getElementById("deuxiemeGroupeExemple3");
+let audioDeuxiemeGroupeExemple3 = document.getElementById(
+  "audioDeuxiemeGroupeExemple3"
+);
+
+deuxiemeGroupeExemple3.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple3.play();
+});
+
+let buttonStop13 = document.getElementById("buttonStop13");
+
+buttonStop13.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple3.pause();
+  audioDeuxiemeGroupeExemple3.currentTime = 0;
+});
+
+let deuxiemeGroupeExemple4 = document.getElementById("deuxiemeGroupeExemple4");
+let audioDeuxiemeGroupeExemple4 = document.getElementById(
+  "audioDeuxiemeGroupeExemple4"
+);
+
+deuxiemeGroupeExemple4.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple4.play();
+});
+
+let buttonStop14 = document.getElementById("buttonStop14");
+
+buttonStop14.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple4.pause();
+  audioDeuxiemeGroupeExemple4.currentTime = 0;
+});
+
+let deuxiemeGroupeExemple5 = document.getElementById("deuxiemeGroupeExemple5");
+let audioDeuxiemeGroupeExemple5 = document.getElementById(
+  "audioDeuxiemeGroupeExemple5"
+);
+
+deuxiemeGroupeExemple5.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple5.play();
+});
+
+let buttonStop15 = document.getElementById("buttonStop15");
+
+buttonStop15.addEventListener("click", () => {
+  audioDeuxiemeGroupeExemple5.pause();
+  audioDeuxiemeGroupeExemple5.currentTime = 0;
+});
+
+/* exemples troisieme groupe */
+
+let troisiemeGroupeExemple1 = document.getElementById(
+  "troisiemeGroupeExemple1"
+);
+let audioTroisiemeGroupeExemple1 = document.getElementById(
+  "audioTroisiemeGroupeExemple1"
+);
+
+troisiemeGroupeExemple1.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple1.play();
+});
+
+let buttonStop16 = document.getElementById("buttonStop16");
+
+buttonStop16.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple1.pause();
+  audioTroisiemeGroupeExemple1.currentTime = 0;
+});
+
+let troisiemeGroupeExemple2 = document.getElementById(
+  "troisiemeGroupeExemple2"
+);
+let audioTroisiemeGroupeExemple2 = document.getElementById(
+  "audioTroisiemeGroupeExemple2"
+);
+
+troisiemeGroupeExemple2.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple2.play();
+});
+
+let buttonStop17 = document.getElementById("buttonStop17");
+
+buttonStop17.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple2.pause();
+  audioTroisiemeGroupeExemple2.currentTime = 0;
+});
+
+let troisiemeGroupeExemple3 = document.getElementById(
+  "troisiemeGroupeExemple3"
+);
+let audioTroisiemeGroupeExemple3 = document.getElementById(
+  "audioTroisiemeGroupeExemple3"
+);
+
+troisiemeGroupeExemple3.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple3.play();
+});
+
+let buttonStop18 = document.getElementById("buttonStop18");
+
+buttonStop18.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple3.pause();
+  audioTroisiemeGroupeExemple3.currentTime = 0;
+});
+
+let troisiemeGroupeExemple4 = document.getElementById(
+  "troisiemeGroupeExemple4"
+);
+let audioTroisiemeGroupeExemple4 = document.getElementById(
+  "audioTroisiemeGroupeExemple4"
+);
+
+troisiemeGroupeExemple4.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple4.play();
+});
+
+let buttonStop19 = document.getElementById("buttonStop19");
+
+buttonStop19.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple4.pause();
+  audioTroisiemeGroupeExemple4.currentTime = 0;
+});
+
+let troisiemeGroupeExemple5 = document.getElementById(
+  "troisiemeGroupeExemple5"
+);
+let audioTroisiemeGroupeExemple5 = document.getElementById(
+  "audioTroisiemeGroupeExemple5"
+);
+
+troisiemeGroupeExemple5.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple5.play();
+});
+
+let buttonStop20 = document.getElementById("buttonStop20");
+
+buttonStop20.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple5.pause();
+  audioTroisiemeGroupeExemple5.currentTime = 0;
+});
+
+let troisiemeGroupeExemple6 = document.getElementById(
+  "troisiemeGroupeExemple6"
+);
+let audioTroisiemeGroupeExemple6 = document.getElementById(
+  "audioTroisiemeGroupeExemple6"
+);
+
+troisiemeGroupeExemple6.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple6.play();
+});
+
+let buttonStop21 = document.getElementById("buttonStop21");
+
+buttonStop21.addEventListener("click", () => {
+  audioTroisiemeGroupeExemple6.pause();
+  audioTroisiemeGroupeExemple6.currentTime = 0;
+});
+
+/* aller */
+
+let aller1 = document.getElementById("aller1");
+let audioAller1 = document.getElementById("audioAller1");
+
+aller1.addEventListener("click", () => {
+  audioAller1.play();
+});
+
+let aller2 = document.getElementById("aller2");
+let audioAller2 = document.getElementById("audioAller2");
+
+aller2.addEventListener("click", () => {
+  audioAller2.play();
+});
+
+let aller3 = document.getElementById("aller3");
+let audioAller3 = document.getElementById("audioAller3");
+
+aller3.addEventListener("click", () => {
+  audioAller3.play();
+});
+
+let aller4 = document.getElementById("aller4");
+let audioAller4 = document.getElementById("audioAller4");
+
+aller4.addEventListener("click", () => {
+  audioAller4.play();
+});
+
+let aller5 = document.getElementById("aller5");
+let audioAller5 = document.getElementById("audioAller5");
+
+aller5.addEventListener("click", () => {
+  audioAller5.play();
+});
+
+let aller6 = document.getElementById("aller6");
+let audioAller6 = document.getElementById("audioAller6");
+
+aller6.addEventListener("click", () => {
+  audioAller6.play();
+});
+
+let aller7 = document.getElementById("aller7");
+let audioAller7 = document.getElementById("audioAller7");
+
+aller7.addEventListener("click", () => {
+  audioAller7.play();
+});
+
+let aller8 = document.getElementById("aller8");
+let audioAller8 = document.getElementById("audioAller8");
+
+aller8.addEventListener("click", () => {
+  audioAller8.play();
+});
+
+let aller9 = document.getElementById("aller9");
+let audioAller9 = document.getElementById("audioAller9");
+
+aller9.addEventListener("click", () => {
+  audioAller9.play();
+});
+
+/* s'asseoir */
+
+let asseoir1 = document.getElementById("asseoir1");
+let audioAsseoir1 = document.getElementById("audioAsseoir1 ");
+
+asseoir1.addEventListener("click", () => {
+  audioAsseoir1.play();
+});
+
+let asseoir2 = document.getElementById("asseoir2");
+let audioAsseoir2 = document.getElementById("audioAsseoir2");
+
+asseoir2.addEventListener("click", () => {
+  audioAsseoir2.play();
+});
+
+let asseoir3 = document.getElementById("asseoir3");
+let audioAsseoir3 = document.getElementById("audioAsseoir3");
+
+asseoir3.addEventListener("click", () => {
+  audioAsseoir3.play();
+});
+
+let asseoir4 = document.getElementById("asseoir4");
+let audioAsseoir4 = document.getElementById("audioAsseoir4");
+
+asseoir4.addEventListener("click", () => {
+  audioAsseoir4.play();
+});
+
+let asseoir5 = document.getElementById("asseoir5");
+let audioAsseoir5 = document.getElementById("audioAsseoir5");
+
+asseoir5.addEventListener("click", () => {
+  audioAsseoir5.play();
+});
+
+let asseoir6 = document.getElementById("asseoir6");
+let audioAsseoir6 = document.getElementById("audioAsseoir6");
+
+asseoir6.addEventListener("click", () => {
+  audioAsseoir6.play();
+});
+
+let asseoir7 = document.getElementById("asseoir7");
+let audioAsseoir7 = document.getElementById("audioAsseoir7");
+
+asseoir7.addEventListener("click", () => {
+  audioAsseoir7.play();
+});
+
+let asseoir8 = document.getElementById("asseoir8");
+let audioAsseoir8 = document.getElementById("audioAsseoir8");
+
+asseoir8.addEventListener("click", () => {
+  audioAsseoir8.play();
+});
+
+let asseoir9 = document.getElementById("asseoir9");
+let audioAsseoir9 = document.getElementById("audioAsseoir9");
+
+asseoir9.addEventListener("click", () => {
+  audioAsseoir9.play();
+});
+
+/* croire */
+
+let croire1 = document.getElementById("croire1");
+let audioCroire1 = document.getElementById("audioCroire1");
+
+croire1.addEventListener("click", () => {
+  audioCroire1.play();
+});
+
+let croire2 = document.getElementById("croire2");
+let audioCroire2 = document.getElementById("audioCroire2");
+
+croire2.addEventListener("click", () => {
+  audioCroire2.play();
+});
+
+let croire3 = document.getElementById("croire3");
+let audioCroire3 = document.getElementById("audioCroire3");
+
+croire3.addEventListener("click", () => {
+  audioCroire3.play();
+});
+
+let croire4 = document.getElementById("croire4");
+let audioCroire4 = document.getElementById("audioCroire4");
+
+croire4.addEventListener("click", () => {
+  audioCroire4.play();
+});
+
+let croire5 = document.getElementById("croire5");
+let audioCroire5 = document.getElementById("audioCroire5");
+
+croire5.addEventListener("click", () => {
+  audioCroire5.play();
+});
+
+let croire6 = document.getElementById("croire6");
+let audioCroire6 = document.getElementById("audioCroire6");
+
+croire6.addEventListener("click", () => {
+  audioCroire6.play();
+});
+
+let croire7 = document.getElementById("croire7");
+let audioCroire7 = document.getElementById("audioCroire7");
+
+croire7.addEventListener("click", () => {
+  audioCroire7.play();
+});
+
+let croire8 = document.getElementById("croire8");
+let audioCroire8 = document.getElementById("audioCroire8");
+
+croire8.addEventListener("click", () => {
+  audioCroire8.play();
+});
+
+let croire9 = document.getElementById("croire9");
+let audioCroire9 = document.getElementById("audioCroire9");
+
+croire9.addEventListener("click", () => {
+  audioCroire9.play();
+});
