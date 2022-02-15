8b8cca37d3880bfd162812a4f731560bccd2cca9
nabila hajou <nab.ikk13@gmail.com>
2022-02-15 10:54:18 +0100

    added audios via js niveauA1lecon6 js file

---
+++ niveauA1lecon6.js
@@ -0,0 +1,276 @@
+/* aimer */
+let aimer1 = document.getElementById("aimer1");
+let audioAimer1 = document.getElementById("audioAimer1");
+
+aimer1.addEventListener("click", () => {
+  audioAimer1.play();
+});
+
+let aimer2 = document.getElementById("aimer2");
+let audioAimer2 = document.getElementById("audioAimer2");
+
+aimer2.addEventListener("click", () => {
+  audioAimer2.play();
+});
+
+let aimer3 = document.getElementById("aimer3");
+let audioAimer3 = document.getElementById("audioAimer3");
+
+aimer3.addEventListener("click", () => {
+  audioAimer3.play();
+});
+
+let aimer4 = document.getElementById("aimer4");
+let audioAimer4 = document.getElementById("audioAimer4");
+
+aimer4.addEventListener("click", () => {
+  audioAimer4.play();
+});
+
+let aimer5 = document.getElementById("aimer5");
+let audioAimer5 = document.getElementById("audioAimer5");
+
+aimer5.addEventListener("click", () => {
+  audioAimer5.play();
+});
+
+let aimer6 = document.getElementById("aimer6");
+let audioAimer6 = document.getElementById("audioAimer6");
+
+aimer6.addEventListener("click", () => {
+  audioAimer6.play();
+});
+
+let aimer7 = document.getElementById("aimer7");
+let audioAimer7 = document.getElementById("audioAimer7");
+
+aimer7.addEventListener("click", () => {
+  audioAimer7.play();
+});
+
+let aimer8 = document.getElementById("aimer8");
+let audioAimer8 = document.getElementById("audioAimer8");
+
+aimer8.addEventListener("click", () => {
+  audioAimer8.play();
+});
+
+/* exemples aimer */
+
+let aimerExemple1 = document.getElementById("aimerExemple1");
+let audioAimerExemple1 = document.getElementById("audioAimerExemple1");
+
+aimerExemple1.addEventListener("click", () => {
+  audioAimerExemple1.play();
+});
+
+let aimerExemple2 = document.getElementById("aimerExemple2");
+let audioAimerExemple2 = document.getElementById("audioAimerExemple2");
+
+aimerExemple2.addEventListener("click", () => {
+  audioAimerExemple2.play();
+});
+
+let aimerExemple3 = document.getElementById("aimerExemple3");
+let audioAimerExemple3 = document.getElementById("audioAimerExemple3");
+
+aimerExemple3.addEventListener("click", () => {
+  audioAimerExemple3.play();
+});
+
+let aimerExemple5 = document.getElementById("aimerExemple5");
+let audioAimerExemple5 = document.getElementById("audioAimerExemple5");
+
+aimerExemple5.addEventListener("click", () => {
+  audioAimerExemple5.play();
+});
+
+/* preferer */
+
+let preferer1 = document.getElementById("preferer1");
+let audioPreferer1 = document.getElementById("audioPreferer1");
+
+preferer1.addEventListener("click", () => {
+  audioPreferer1.play();
+});
+
+let preferer2 = document.getElementById("preferer2");
+let audioPreferer2 = document.getElementById("audioPreferer2");
+
+preferer2.addEventListener("click", () => {
+  audioPreferer2.play();
+});
+
+let preferer3 = document.getElementById("preferer3");
+let audioPreferer3 = document.getElementById("audioPreferer3");
+
+preferer3.addEventListener("click", () => {
+  audioPreferer3.play();
+});
+
+let preferer4 = document.getElementById("preferer4");
+let audioPreferer4 = document.getElementById("audioPreferer4");
+
+preferer4.addEventListener("click", () => {
+  audioPreferer4.play();
+});
+
+let preferer5 = document.getElementById("preferer5");
+let audioPreferer5 = document.getElementById("audioPreferer5");
+
+preferer5.addEventListener("click", () => {
+  audioPreferer5.play();
+});
+
+let preferer6 = document.getElementById("preferer6");
+let audioPreferer6 = document.getElementById("audioPreferer6");
+
+preferer6.addEventListener("click", () => {
+  audioPreferer6.play();
+});
+
+let preferer7 = document.getElementById("preferer7");
+let audioPreferer7 = document.getElementById("audioPreferer7");
+
+preferer7.addEventListener("click", () => {
+  audioPreferer7.play();
+});
+
+let preferer8 = document.getElementById("preferer8");
+let audioPreferer8 = document.getElementById("audioPreferer8");
+
+preferer8.addEventListener("click", () => {
+  audioPreferer8.play();
+});
+
+/* exemples preferer */
+
+let prefererExemple1 = document.getElementById("prefererExemple1");
+let audioPrefererExemple1 = document.getElementById("audioPrefererExemple1");
+
+prefererExemple1.addEventListener("click", () => {
+  audioPrefererExemple1.play();
+});
+
+let prefererExemple2 = document.getElementById("prefererExemple2");
+let audioPrefererExemple2 = document.getElementById("audioPrefererExemple2");
+
+prefererExemple2.addEventListener("click", () => {
+  audioPrefererExemple2.play();
+});
+
+let prefererExemple3 = document.getElementById("prefererExemple3");
+let audioPrefererExemple3 = document.getElementById("audioPrefererExemple3");
+
+prefererExemple3.addEventListener("click", () => {
+  audioPrefererExemple3.play();
+});
+
+let prefererExemple4 = document.getElementById("prefererExemple4");
+let audioPrefererExemple4 = document.getElementById("audioPrefererExemple4");
+
+prefererExemple4.addEventListener("click", () => {
+  audioPrefererExemple4.play();
+});
+
+let prefererExemple5 = document.getElementById("prefererExemple5");
+let audioPrefererExemple5 = document.getElementById("audioPrefererExemple5");
+
+prefererExemple5.addEventListener("click", () => {
+  audioPrefererExemple5.play();
+});
+
+/* détester */
+
+let detester1 = document.getElementById("detester1");
+let audioDetester1 = document.getElementById("audioDetester1");
+
+detester1.addEventListener("click", () => {
+  audioDetester1.play();
+});
+
+let detester2 = document.getElementById("detester2");
+let audioDetester2 = document.getElementById("audioDetester2");
+
+detester2.addEventListener("click", () => {
+  audioDetester2.play();
+});
+
+let detester3 = document.getElementById("detester3");
+let audioDetester3 = document.getElementById("audioDetester3");
+
+detester3.addEventListener("click", () => {
+  audioDetester3.play();
+});
+
+let detester4 = document.getElementById("detester4");
+let audioDetester4 = document.getElementById("audioDetester4");
+
+detester4.addEventListener("click", () => {
+  audioDetester4.play();
+});
+
+let detester5 = document.getElementById("detester5");
+let audioDetester5 = document.getElementById("audioDetester5");
+
+detester5.addEventListener("click", () => {
+  audioDetester5.play();
+});
+
+let detester6 = document.getElementById("detester6");
+let audioDetester6 = document.getElementById("audioDetester6");
+
+detester6.addEventListener("click", () => {
+  audioDetester6.play();
+});
+
+let detester7 = document.getElementById("detester7");
+let audioDetester7 = document.getElementById("audioDetester7");
+
+detester7.addEventListener("click", () => {
+  audioDetester7.play();
+});
+
+let detester8 = document.getElementById("detester8");
+let audioDetester8 = document.getElementById("audioDetester8");
+
+detester8.addEventListener("click", () => {
+  audioDetester8.play();
+});
+
+/* exemples detester */
+
+let detesterExemple1 = document.getElementById("detesterExemple1");
+let audioDetesterExemple1 = document.getElementById("audioDetesterExemple1");
+
+detesterExemple1.addEventListener("click", () => {
+  audioDetesterExemple1.play();
+});
+
+let detesterExemple2 = document.getElementById("detesterExemple2");
+let audioDetesterExemple2 = document.getElementById("audioDetesterExemple2");
+
+detesterExemple2.addEventListener("click", () => {
+  audioDetesterExemple2.play();
+});
+
+let detesterExemple3 = document.getElementById("detesterExemple3");
+let audioDetesterExemple3 = document.getElementById("audioDetesterExemple3");
+
+detesterExemple3.addEventListener("click", () => {
+  audioDetesterExemple3.play();
+});
+
+let detesterExemple4 = document.getElementById("detesterExemple4");
+let audioDetesterExemple4 = document.getElementById("audioDetesterExemple4");
+
+detesterExemple4.addEventListener("click", () => {
+  audioDetesterExemple4.play();
+});
+
+let detesterExemple5 = document.getElementById("detesterExemple5");
+let audioDetesterExemple5 = document.getElementById("audioDetesterExemple5");
+
+detesterExemple5.addEventListener("click", () => {
+  audioDetesterExemple5.play();
+});
