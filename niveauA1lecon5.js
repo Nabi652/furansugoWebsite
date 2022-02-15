5b8af73d8f30e9078fbcb32145dde20799160a1a
nabila hajou <nab.ikk13@gmail.com>
2022-02-15 04:00:32 +0100

    added audios via js niveaua1lecon5

---
+++ niveauA1lecon5.js
@@ -0,0 +1,103 @@
+/* etre */
+
+let etre1 = document.getElementById("etre1");
+let audioEtre1 = document.getElementById("audioEtre1");
+
+etre1.addEventListener("click", () => {
+  audioEtre1.play();
+});
+
+let etre2 = document.getElementById("etre2");
+let audioEtre2 = document.getElementById("audioEtre2");
+
+etre2.addEventListener("click", () => {
+  audioEtre2.play();
+});
+
+let etre3 = document.getElementById("etre3");
+let audioEtre3 = document.getElementById("audioEtre3");
+
+etre3.addEventListener("click", () => {
+  audioEtre3.play();
+});
+
+let etre4 = document.getElementById("etre4");
+let audioEtre4 = document.getElementById("audioEtre4");
+
+etre4.addEventListener("click", () => {
+  audioEtre4.play();
+});
+
+let etre5 = document.getElementById("etre5");
+let audioEtre5 = document.getElementById("audioEtre5");
+
+etre5.addEventListener("click", () => {
+  audioEtre5.play();
+});
+
+let etre6 = document.getElementById("etre6");
+let audioEtre6 = document.getElementById("audioEtre6");
+
+etre6.addEventListener("click", () => {
+  audioEtre6.play();
+});
+
+let etre7 = document.getElementById("etre7");
+let audioEtre7 = document.getElementById("audioEtre7");
+
+etre7.addEventListener("click", () => {
+  audioEtre7.play();
+});
+
+let etre8 = document.getElementById("etre8");
+let audioEtre8 = document.getElementById("audioEtre8");
+
+etre8.addEventListener("click", () => {
+  audioEtre8.play();
+});
+
+/* exemples etre*/
+
+let etreExemple1 = document.getElementById("etreExemple1");
+let audioEtreExemple1 = document.getElementById("audioEtreExemple1");
+
+etreExemple1.addEventListener("click", () => {
+  audioEtreExemple1.play();
+});
+
+let etreExemple2 = document.getElementById("etreExemple2");
+let audioEtreExemple2 = document.getElementById("audioEtreExemple2");
+
+etreExemple2.addEventListener("click", () => {
+  audioEtreExemple2.play();
+});
+
+let etreExemple3 = document.getElementById("etreExemple3");
+let audioEtreExemple3 = document.getElementById("audioEtreExemple3");
+
+etreExemple3.addEventListener("click", () => {
+  audioEtreExemple3.play();
+});
+
+let etreExemple4 = document.getElementById("etreExemple4");
+let audioEtreExemple4 = document.getElementById("audioEtreExemple4");
+
+etreExemple4.addEventListener("click", () => {
+  audioEtreExemple4.play();
+});
+
+let etreExemple5 = document.getElementById("etreExemple5");
+let audioEtreExemple5 = document.getElementById("audioEtreExemple5");
+
+etreExemple5.addEventListener("click", () => {
+  audioEtreExemple5.play();
+});
+
+/* avoir */
+
+let avoir1 = document.getElementById("avoir1");
+let audioAvoir1 = document.getElementById("audioAvoir1");
+
+avoir1.addEventListener("click", () => {
+  audioAvoir1.play();
+});
