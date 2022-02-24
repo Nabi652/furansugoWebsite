bf9ab48387f1fc88fe51e4af8c2c615989908ac4
nabila hajou <nab.ikk13@gmail.com>
2022-02-24 11:36:04 +0100

    added audios visa niveaua1lecon7 js file

--- niveauA1lecon7.js
+++ niveauA1lecon7.js
@@ -1,6 +1,63 @@
+/* faire */
 let faire0 = document.getElementById("faire0");
 let audioFaire0 = document.getElementById("audioFaire0");
 
 faire0.addEventListener("click", () => {
   audioFaire0.play();
+});
+
+let faire1 = document.getElementById("faire1");
+let audioFaire1 = document.getElementById("audioFaire1");
+
+faire1.addEventListener("click", () => {
+  audioFaire1.play();
+});
+
+let faire2 = document.getElementById("faire2");
+let audioFaire2 = document.getElementById("audioFaire2");
+
+faire2.addEventListener("click", () => {
+  audioFaire2.play();
+});
+
+let faire3 = document.getElementById("faire3");
+let audioFaire3 = document.getElementById("audioFaire3");
+
+faire3.addEventListener("click", () => {
+  audioFaire3.play();
+});
+
+let faire4 = document.getElementById("faire4");
+let audioFaire4 = document.getElementById("audioFaire4");
+
+faire4.addEventListener("click", () => {
+  audioFaire4.play();
+});
+
+let faire5 = document.getElementById("faire5");
+let audioFaire5 = document.getElementById("audioFaire5");
+
+faire5.addEventListener("click", () => {
+  audioFaire5.play();
+});
+
+let faire6 = document.getElementById("faire6");
+let audioFaire6 = document.getElementById("audioFaire6");
+
+faire6.addEventListener("click", () => {
+  audioFaire6.play();
+});
+
+let faire7 = document.getElementById("faire7");
+let audioFaire7 = document.getElementById("audioFaire7");
+
+faire7.addEventListener("click", () => {
+  audioFaire7.play();
+});
+
+let faire8 = document.getElementById("faire8");
+let audioFaire8 = document.getElementById("audioFaire8");
+
+faire8.addEventListener("click", () => {
+  audioFaire.play();
 });
