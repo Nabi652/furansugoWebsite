64d48a8a0a238efc7fcf5f02a7c8720e96bcbc51
nabila hajou <nab.ikk13@gmail.com>
2022-02-25 03:59:39 +0100

    added audios via js niveaua1lecon7 js file

--- niveauA1lecon7.js
+++ niveauA1lecon7.js
@@ -59,5 +59,276 @@ let faire8 = document.getElementById("faire8");
 let audioFaire8 = document.getElementById("audioFaire8");
 
 faire8.addEventListener("click", () => {
-  audioFaire.play();
+  audioFaire8.play();
 });
+
+/* savoir */
+
+let savoir0 = document.getElementById("savoir0");
+let audioSavoir0 = document.getElementById("audioSavoir0");
+
+savoir0.addEventListener("click", () => {
+  audioSavoir0.play();
+});
+
+let savoir1 = document.getElementById("savoir1");
+let audioSavoir1 = document.getElementById("audioSavoir1");
+
+savoir1.addEventListener("click", () => {
+  audioSavoir1.play();
+});
+
+let savoir2 = document.getElementById("savoir2");
+let audioSavoir2 = document.getElementById("audioSavoir2");
+
+savoir2.addEventListener("click", () => {
+  audioSavoir2.play();
+});
+
+let savoir3 = document.getElementById("savoir3");
+let audioSavoir3 = document.getElementById("audioSavoir3");
+
+savoir3.addEventListener("click", () => {
+  audioSavoir3.play();
+});
+
+let savoir4 = document.getElementById("savoir4");
+let audioSavoir4 = document.getElementById("audioSavoir4");
+
+savoir4.addEventListener("click", () => {
+  audioSavoir4.play();
+});
+
+let savoir5 = document.getElementById("savoir5");
+let audioSavoir5 = document.getElementById("audioSavoir5");
+
+savoir5.addEventListener("click", () => {
+  audioSavoir5.play();
+});
+
+let savoir6 = document.getElementById("savoir6");
+let audioSavoir6 = document.getElementById("audioSavoir6");
+
+savoir6.addEventListener("click", () => {
+  audioSavoir6.play();
+});
+
+let savoir7 = document.getElementById("savoir7");
+let audioSavoir7 = document.getElementById("audioSavoir7");
+
+savoir7.addEventListener("click", () => {
+  audioSavoir7.play();
+});
+
+let savoir8 = document.getElementById("savoir8");
+let audioSavoir8 = document.getElementById("audioSavoir8");
+
+savoir8.addEventListener("click", () => {
+  audioSavoir8.play();
+});
+
+/* dire*/
+let dire0 = document.getElementById("dire0");
+let audioDire0 = document.getElementById("audioDire0");
+
+dire0.addEventListener("click", () => {
+  audioDire0.play();
+});
+
+let dire1 = document.getElementById("dire1");
+let audioDire1 = document.getElementById("audioDire1");
+
+dire1.addEventListener("click", () => {
+  audioDire1.play();
+});
+
+let dire2 = document.getElementById("dire2");
+let audioDire2 = document.getElementById("audioDire2");
+
+dire2.addEventListener("click", () => {
+  audioDire2.play();
+});
+
+let dire3 = document.getElementById("dire3");
+let audioDire3 = document.getElementById("audioDire3");
+
+dire3.addEventListener("click", () => {
+  audioDire3.play();
+});
+
+let dire4 = document.getElementById("dire4");
+let audioDire4 = document.getElementById("audioDire4");
+
+dire4.addEventListener("click", () => {
+  audioDire4.play();
+});
+
+let dire5 = document.getElementById("dire5");
+let audioDire5 = document.getElementById("audioDire5");
+
+dire5.addEventListener("click", () => {
+  audioDire5.play();
+});
+
+let dire6 = document.getElementById("dire6");
+let audioDire6 = document.getElementById("audioDire6");
+
+dire6.addEventListener("click", () => {
+  audioDire6.play();
+});
+
+let dire7 = document.getElementById("dire7");
+let audioDire7 = document.getElementById("audioDire7");
+
+dire7.addEventListener("click", () => {
+  audioDire7.play();
+});
+
+let dire8 = document.getElementById("dire8");
+let audioDire8 = document.getElementById("audioDire8");
+
+dire8.addEventListener("click", () => {
+  audioDire8.play();
+});
+/*voir*/
+let voir0 = document.getElementById("voir0");
+let audioVoir0 = document.getElementById("audioVoir0");
+
+voir0.addEventListener("click", () => {
+  audioVoir0.play();
+});
+
+let voir1 = document.getElementById("voir1");
+let audioVoir1 = document.getElementById("audioVoir1");
+
+voir1.addEventListener("click", () => {
+  audioVoir1.play();
+});
+
+let voir2 = document.getElementById("voir2");
+let audioVoir2 = document.getElementById("audioVoir2");
+
+voir2.addEventListener("click", () => {
+  audioVoir2.play();
+});
+
+let voir3 = document.getElementById("voir3");
+let audioVoir3 = document.getElementById("audioVoir3");
+
+voir3.addEventListener("click", () => {
+  audioVoir3.play();
+});
+
+let voir4 = document.getElementById("voir4");
+let audioVoir4 = document.getElementById("audioVoir4");
+
+voir4.addEventListener("click", () => {
+  audioVoir4.play();
+});
+
+let voir5 = document.getElementById("voir5");
+let audioVoir5 = document.getElementById("audioVoir5");
+
+voir5.addEventListener("click", () => {
+  audioVoir5.play();
+});
+
+let voir6 = document.getElementById("voir6");
+let audioVoir6 = document.getElementById("audioVoir6");
+
+voir6.addEventListener("click", () => {
+  audioVoir6.play();
+});
+
+let voir7 = document.getElementById("voir7");
+let audioVoir7 = document.getElementById("audioVoir7");
+
+voir7.addEventListener("click", () => {
+  audioVoir7.play();
+});
+
+let voir8 = document.getElementById("voir8");
+let audioVoir8 = document.getElementById("audioVoir8");
+
+voir8.addEventListener("click", () => {
+  audioVoir8.play();
+});
+/*regarder*/
+let regarder0 = document.getElementById("regarder0");
+let audioRegarder0 = document.getElementById("audioRegarder0");
+
+regarder0.addEventListener("click", () => {
+  audioRegarder0.play();
+});
+
+let regarder1 = document.getElementById("regarder1");
+let audioRegarder1 = document.getElementById("audioRegarder1");
+
+regarder1.addEventListener("click", () => {
+  audioRegarder1.play();
+});
+
+let regarder2 = document.getElementById("regarder2");
+let audioRegarder2 = document.getElementById("audioRegarder2");
+
+regarder2.addEventListener("click", () => {
+  audioRegarder2.play();
+});
+
+let regarder3 = document.getElementById("regarder3");
+let audioRegarder3 = document.getElementById("audioRegarder3");
+
+regarder3.addEventListener("click", () => {
+  audioRegarder3.play();
+});
+
+let regarder4 = document.getElementById("regarder4");
+let audioRegarder4 = document.getElementById("audioRegarder4");
+
+regarder4.addEventListener("click", () => {
+  audioRegarder4.play();
+});
+
+let regarder5 = document.getElementById("regarder5");
+let audioRegarder5 = document.getElementById("audioRegarder5");
+
+regarder5.addEventListener("click", () => {
+  audioRegarder5.play();
+});
+
+let regarder6 = document.getElementById("regarder6");
+let audioRegarder6 = document.getElementById("audioRegarder6");
+
+regarder6.addEventListener("click", () => {
+  audioRegarder6.play();
+});
+
+let regarder7 = document.getElementById("regarder7");
+let audioRegarder7 = document.getElementById("audioRegarder7");
+
+regarder7.addEventListener("click", () => {
+  audioRegarder7.play();
+});
+
+let regarder8 = document.getElementById("regarder8");
+let audioRegarder8 = document.getElementById("audioRegarder8");
+
+regarder8.addEventListener("click", () => {
+  audioRegarder8.play();
+});
+/*prendre*/
+/*pouvoir*/
+/*mettre*/
+/*trouver*/
+/*parler*/
+/*venir*/
+/*aller*/
+/*passer*/
+/*rendre*/
+/*laisser*/
+/*vivre*/
+/*demander*/
+/*attendre*/
+/*comprendre*/
+/*ecrire*/
+/*jouer*/
