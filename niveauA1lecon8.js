0b6870a45b9a53bc74448087e51550da8b68155e
nabila hajou <nab.ikk13@gmail.com>
2022-02-26 14:17:13 +0100

    added audios of faire les courses part in niveaua1lecon8 js file

---
+++ niveauA1lecon8.js
@@ -0,0 +1,299 @@
+/*faire les courses */
+let lesCourses1 = document.getElementById("lesCourses1");
+let audioLesCourses1 = document.getElementById("audioLesCourses1");
+
+lesCourses1.addEventListener("click", () => {
+  audioLesCourses1.play();
+});
+
+let lesCourses2 = document.getElementById("lesCourse2");
+let audioLesCourses2 = document.getElementById("audioLesCourses2");
+
+lesCourses2.addEventListener("click", () => {
+  audioLesCourses2.play();
+});
+
+let lesCourses3 = document.getElementById("lesCourses3");
+let audioLesCourses3 = document.getElementById("audioLesCourses3");
+
+lesCourses3.addEventListener("click", () => {
+  audioLesCourses3.play();
+});
+
+let lesCourses4 = document.getElementById("lesCourses4");
+let audioLesCourses4 = document.getElementById("audioLesCourses4");
+
+lesCourses4.addEventListener("click", () => {
+  audioLesCourses4.play();
+});
+
+let lesCourses5 = document.getElementById("lesCourses5");
+let audioLesCourses5 = document.getElementById("audioLesCourses5");
+
+lesCourses5.addEventListener("click", () => {
+  audioLesCourses5.play();
+});
+
+let lesCourses6 = document.getElementById("lesCourses6");
+let audioLesCourses6 = document.getElementById("audioLesCourses6");
+
+lesCourses6.addEventListener("click", () => {
+  audioLesCourses6.play();
+});
+
+let lesCourses7 = document.getElementById("lesCourses7");
+let audioLesCourses7 = document.getElementById("audioLesCourses7");
+
+lesCourses7.addEventListener("click", () => {
+  audioLesCourses7.play();
+});
+
+let lesCourses8 = document.getElementById("lesCourses8");
+let audioLesCourses8 = document.getElementById("audioLesCourses8");
+
+lesCourses8.addEventListener("click", () => {
+  audioLesCourses8.play();
+});
+
+/* exemples*/
+let lesCoursesExemple1 = document.getElementById("lesCoursesExemple1");
+let audioLesCoursesExemple1 = document.getElementById(
+  "audioLesCoursesExemple1"
+);
+
+lesCoursesExemple1.addEventListener("click", () => {
+  audioLesCoursesExemple1.play();
+});
+
+let lesCoursesExemple2 = document.getElementById("lesCoursesExemple2");
+let audioLesCoursesExemple2 = document.getElementById(
+  "audioLesCoursesExemple2"
+);
+
+lesCoursesExemple2.addEventListener("click", () => {
+  audioLesCoursesExemple2.play();
+});
+
+let lesCoursesExemple3 = document.getElementById("lesCoursesExemple3");
+let audioLesCoursesExemple3 = document.getElementById(
+  "audioLesCoursesExemple3"
+);
+
+lesCoursesExemple3.addEventListener("click", () => {
+  audioLesCoursesExemple3.play();
+});
+let lesCoursesExemple4 = document.getElementById("lesCoursesExemple4");
+let audioLesCoursesExemple4 = document.getElementById(
+  "audioLesCoursesExemple4"
+);
+
+lesCoursesExemple4.addEventListener("click", () => {
+  audioLesCoursesExemple4.play();
+});
+
+let lesCoursesExemple5 = document.getElementById("lesCoursesExemple5");
+let audioLesCoursesExemple5 = document.getElementById(
+  "audioLesCoursesExemple5"
+);
+
+lesCoursesExemple5.addEventListener("click", () => {
+  audioLesCoursesExemple5.play();
+});
+
+let lesCoursesExemple6 = document.getElementById("lesCoursesExemple6");
+let audioLesCoursesExemple6 = document.getElementById(
+  "audioLesCoursesExemple6"
+);
+
+lesCoursesExemple6.addEventListener("click", () => {
+  audioLesCoursesExemple6.play();
+});
+
+let lesCoursesExemple7 = document.getElementById("lesCoursesExemple7");
+let audioLesCoursesExemple7 = document.getElementById(
+  "audioLesCoursesExemple7"
+);
+
+lesCoursesExemple7.addEventListener("click", () => {
+  audioLesCoursesExemple7.play();
+});
+
+let lesCoursesExemple8 = document.getElementById("lesCoursesExemple8");
+let audioLesCoursesExemple8 = document.getElementById(
+  "audioLesCoursesExemple8"
+);
+
+lesCoursesExemple8.addEventListener("click", () => {
+  audioLesCoursesExemple8.play();
+});
+
+let lesCoursesExemple9 = document.getElementById("lesCoursesExemple9");
+let audioLesCoursesExemple9 = document.getElementById(
+  "audioLesCoursesExemple9"
+);
+
+lesCoursesExemple9.addEventListener("click", () => {
+  audioLesCoursesExemple9.play();
+});
+
+let lesCoursesExemple10 = document.getElementById("lesCoursesExemple10");
+let audioLesCoursesExemple10 = document.getElementById(
+  "audioLesCoursesExemple10"
+);
+
+lesCoursesExemple10.addEventListener("click", () => {
+  audioLesCoursesExemple10.play();
+});
+
+let lesCoursesExemple11 = document.getElementById("lesCoursesExemple11");
+let audioLesCoursesExemple11 = document.getElementById(
+  "audioLesCoursesExemple11"
+);
+
+lesCoursesExemple11.addEventListener("click", () => {
+  audioLesCoursesExemple11.play();
+});
+
+let lesCoursesExemple12 = document.getElementById("lesCoursesExemple12");
+let audioLesCoursesExemple12 = document.getElementById(
+  "audioLesCoursesExemple12"
+);
+
+lesCoursesExemple12.addEventListener("click", () => {
+  audioLesCoursesExemple12.play();
+});
+
+let lesCoursesExemple13 = document.getElementById("lesCoursesExemple13");
+let audioLesCoursesExemple13 = document.getElementById(
+  "audioLesCoursesExemple13"
+);
+
+lesCoursesExemple13.addEventListener("click", () => {
+  audioLesCoursesExemple13.play();
+});
+
+let lesCoursesExemple14 = document.getElementById("lesCoursesExemple14");
+let audioLesCoursesExemple14 = document.getElementById(
+  "audioLesCoursesExemple14"
+);
+
+lesCoursesExemple14.addEventListener("click", () => {
+  audioLesCoursesExemple14.play();
+});
+
+let lesCoursesExemple15 = document.getElementById("lesCoursesExemple15");
+let audioLesCoursesExemple15 = document.getElementById(
+  "audioLesCoursesExemple15"
+);
+
+lesCoursesExemple15.addEventListener("click", () => {
+  audioLesCoursesExemple15.play();
+});
+
+let lesCoursesExemple16 = document.getElementById("lesCoursesExemple16");
+let audioLesCoursesExemple16 = document.getElementById(
+  "audioLesCoursesExemple16"
+);
+
+lesCoursesExemple16.addEventListener("click", () => {
+  audioLesCoursesExemple16.play();
+});
+
+let lesCoursesExemple17 = document.getElementById("lesCoursesExemple17");
+let audioLesCoursesExemple17 = document.getElementById(
+  "audioLesCoursesExemple17"
+);
+
+lesCoursesExemple17.addEventListener("click", () => {
+  audioLesCoursesExemple17.play();
+});
+
+let lesCoursesExemple18 = document.getElementById("lesCoursesExemple18");
+let audioLesCoursesExemple18 = document.getElementById(
+  "audioLesCoursesExemple18"
+);
+
+lesCoursesExemple18.addEventListener("click", () => {
+  audioLesCoursesExemple18.play();
+});
+
+let lesCoursesExemple19 = document.getElementById("lesCoursesExemple19");
+let audioLesCoursesExemple19 = document.getElementById(
+  "audioLesCoursesExemple19"
+);
+
+lesCoursesExemple19.addEventListener("click", () => {
+  audioLesCoursesExemple19.play();
+});
+
+let lesCoursesExemple20 = document.getElementById("lesCoursesExemple20");
+let audioLesCoursesExemple20 = document.getElementById(
+  "audioLesCoursesExemple20"
+);
+
+lesCoursesExemple20.addEventListener("click", () => {
+  audioLesCoursesExemple20.play();
+});
+
+let lesCoursesExemple21 = document.getElementById("lesCoursesExemple21");
+let audioLesCoursesExemple21 = document.getElementById(
+  "audioLesCoursesExemple21"
+);
+
+lesCoursesExemple21.addEventListener("click", () => {
+  audioLesCoursesExemple21.play();
+});
+
+let lesCoursesExemple22 = document.getElementById("lesCoursesExemple22");
+let audioLesCoursesExemple22 = document.getElementById(
+  "audioLesCoursesExemple22"
+);
+
+lesCoursesExemple22.addEventListener("click", () => {
+  audioLesCoursesExemple22.play();
+});
+
+let lesCoursesExemple23 = document.getElementById("lesCoursesExemple23");
+let audioLesCoursesExemple23 = document.getElementById(
+  "audioLesCoursesExemple23"
+);
+
+lesCoursesExemple23.addEventListener("click", () => {
+  audioLesCoursesExemple23.play();
+});
+
+let lesCoursesExemple24 = document.getElementById("lesCoursesExemple24");
+let audioLesCoursesExemple24 = document.getElementById(
+  "audioLesCoursesExemple24"
+);
+
+lesCoursesExemple24.addEventListener("click", () => {
+  audioLesCoursesExemple24.play();
+});
+
+let lesCoursesExemple25 = document.getElementById("lesCoursesExemple25");
+let audioLesCoursesExemple25 = document.getElementById(
+  "audioLesCoursesExemple25"
+);
+
+lesCoursesExemple25.addEventListener("click", () => {
+  audioLesCoursesExemple25.play();
+});
+
+let lesCoursesExemple26 = document.getElementById("lesCoursesExemple26");
+let audioLesCoursesExemple26 = document.getElementById(
+  "audioLesCoursesExemple26"
+);
+
+lesCoursesExemple26.addEventListener("click", () => {
+  audioLesCoursesExemple26.play();
+});
+
+let lesCoursesExemple27 = document.getElementById("lesCoursesExemple27");
+let audioLesCoursesExemple27 = document.getElementById(
+  "audioLesCoursesExemple27"
+);
+
+lesCoursesExemple27.addEventListener("click", () => {
+  audioLesCoursesExemple27.play();
+});
