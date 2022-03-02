3f387d3c618d1559abe95bf5f9f51c4b3782f403
nabila hajou <nab.ikk13@gmail.com>
2022-03-02 15:10:25 +0100

    added audios files via js niveaua1lecon9 js file

---
+++ niveauA1lecon9.js
@@ -0,0 +1,132 @@
+let lecon9Exemple1 = document.getElementById("lecon9Exemple1");
+let audioLecon9Exemple1 = document.getElementById("audioLecon9Exemple1");
+
+lecon9Exemple1.addEventListener("click", () => {
+  audioLecon9Exemple1.play();
+});
+
+let lecon9Exemple2 = document.getElementById("lecon9Exemple2");
+let audioLecon9Exemple2 = document.getElementById("audioLecon9Exemple2");
+
+lecon9Exemple2.addEventListener("click", () => {
+  audioLecon9Exemple2.play();
+});
+
+let lecon9Exemple3 = document.getElementById("lecon9Exemple3");
+let audioLecon9Exemple3 = document.getElementById("audioLecon9Exemple3");
+
+lecon9Exemple3.addEventListener("click", () => {
+  audioLecon9Exemple3.play();
+});
+
+let lecon9Exemple4 = document.getElementById("lecon9Exemple4");
+let audioLecon9Exemple4 = document.getElementById("audioLecon9Exemple4");
+
+lecon9Exemple4.addEventListener("click", () => {
+  audioLecon9Exemple4.play();
+});
+
+let lecon9Exemple5 = document.getElementById("lecon9Exemple5");
+let audioLecon9Exemple5 = document.getElementById("audioLecon9Exemple5");
+
+lecon9Exemple5.addEventListener("click", () => {
+  audioLecon9Exemple5.play();
+});
+
+let lecon9Exemple6 = document.getElementById("lecon9Exemple6");
+let audioLecon9Exemple6 = document.getElementById("audioLecon9Exemple6");
+
+lecon9Exemple6.addEventListener("click", () => {
+  audioLecon9Exemple6.play();
+});
+
+let lecon9Exemple7 = document.getElementById("lecon9Exemple7");
+let audioLecon9Exemple7 = document.getElementById("audioLecon9Exemple7");
+
+lecon9Exemple7.addEventListener("click", () => {
+  audioLecon9Exemple7.play();
+});
+
+let lecon9Exemple8 = document.getElementById("lecon9Exemple8");
+let audioLecon9Exemple8 = document.getElementById("audioLecon9Exemple8");
+
+lecon9Exemple8.addEventListener("click", () => {
+  audioLecon9Exemple8.play();
+});
+
+let lecon9Exemple9 = document.getElementById("lecon9Exemple9");
+let audioLecon9Exemple9 = document.getElementById("audioLecon9Exemple9");
+
+lecon9Exemple9.addEventListener("click", () => {
+  audioLecon9Exemple9.play();
+});
+
+let lecon9Exemple10 = document.getElementById("lecon9Exemple10");
+let audioLecon9Exemple10 = document.getElementById("audioLecon9Exemple10");
+
+lecon9Exemple10.addEventListener("click", () => {
+  audioLecon9Exemple10.play();
+});
+
+let lecon9Exemple11 = document.getElementById("lecon9Exemple11");
+let audioLecon9Exemple11 = document.getElementById("audioLecon9Exemple11");
+
+lecon9Exemple11.addEventListener("click", () => {
+  audioLecon9Exemple11.play();
+});
+
+let lecon9Exemple12 = document.getElementById("lecon9Exemple12");
+let audioLecon9Exemple12 = document.getElementById("audioLecon9Exemple12");
+
+lecon9Exemple12.addEventListener("click", () => {
+  audioLecon9Exemple12.play();
+});
+
+let lecon9Exemple13 = document.getElementById("lecon9Exemple13");
+let audioLecon9Exemple13 = document.getElementById("audioLecon9Exemple13");
+
+lecon9Exemple13.addEventListener("click", () => {
+  audioLecon9Exemple13.play();
+});
+
+let lecon9Exemple14 = document.getElementById("lecon9Exemple14");
+let audioLecon9Exemple14 = document.getElementById("audioLecon9Exemple14");
+
+lecon9Exemple14.addEventListener("click", () => {
+  audioLecon9Exemple14.play();
+});
+
+let lecon9Exemple15 = document.getElementById("lecon9Exemple15");
+let audioLecon9Exemple15 = document.getElementById("audioLecon9Exemple15");
+
+lecon9Exemple15.addEventListener("click", () => {
+  audioLecon9Exemple15.play();
+});
+
+let lecon9Exemple16 = document.getElementById("lecon9Exemple16");
+let audioLecon9Exemple16 = document.getElementById("audioLecon9Exemple16");
+
+lecon9Exemple16.addEventListener("click", () => {
+  audioLecon9Exemple16.play();
+});
+
+let lecon9Exemple17 = document.getElementById("lecon9Exemple17");
+let audioLecon9Exemple17 = document.getElementById("audioLecon9Exemple17");
+
+lecon9Exemple17.addEventListener("click", () => {
+  audioLecon9Exemple17.play();
+});
+
+let lecon9Exemple18 = document.getElementById("lecon9Exemple18");
+let audioLecon9Exemple18 = document.getElementById("audioLecon9Exemple18");
+
+lecon9Exemple18.addEventListener("click", () => {
+  audioLecon9Exemple18.play();
+});
+
+let lecon9Exemple19 = document.getElementById("lecon9Exemple19");
+let audioLecon9Exemple19 = document.getElementById("audioLecon9Exemple19");
+
+lecon9Exemple19.addEventListener("click", () => {
+  audioLecon9Exemple19.play();
+});
